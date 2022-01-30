5539aa3f57c1d22597cd69aaef9cf9cefc2d16dd
Gianluca O <17207260+RandomGgames@users.noreply.github.com>
2022-01-29 19:11:20 -0500

    Update Fabric Mod Copy Updater.py
    - Moved try statements around
    - Fixed could not delete file because it was in use error

--- Fabric Mod Copy Updater.py
+++ Fabric Mod Copy Updater.py
@@ -4,6 +4,7 @@ import configparser
 from zipfile import ZipFile
 import json
 from datetime import datetime
+from pathlib import Path
 
 
 def log(text, print_to_console: bool = True, print_to_log: bool = True):
@@ -39,70 +40,63 @@ else:
 """BUILD MOST UP TO DATE MOD CACHE"""
 log(f"[{datetime.now().strftime('%m/%d/%Y %H:%M:%S:%f')}] - INFO: BUILDING MOST UP TO DATE MOD CACHE...")
 most_updated_mods_cache = {}
-for version in config.sections():
-	most_updated_mods_cache[version] = {}
-	try:
+try:
+	for version in config.sections():
+		most_updated_mods_cache[version] = {}
 		for mod_to_cache in [mod for mod in os.listdir(config[version]['updated_mods_directory']) if mod.endswith(".jar")]:
-			try:
-				path = f"{config[version]['updated_mods_directory']}\\{mod_to_cache}"
+			path = Path(f"{config[version]['updated_mods_directory']}\\{mod_to_cache}")
+			modzip = ZipFile(path, "r")
+			modinfo = modzip.open("fabric.mod.json", "r")
+			info_json = json.load(modinfo, strict=False)
+			mod_ID = info_json["id"]
+			modinfo.close()
+			modzip.close()
 
-				with ZipFile(path, "r") as modzip:
-					with modzip.open("fabric.mod.json", "r") as modinfo:
-						info_json = json.load(modinfo, strict=False)
-						mod_ID = info_json["id"]
-						
-						if mod_ID in most_updated_mods_cache[version]: # If the mod is already in the mod cache
-							# Remove the mod since there are two different versions
-							most_updated_mods_cache[version].pop(mod_ID)
-							log(f"WARN: Duplicate mod '{mod_ID}' found in most updated mods directory '{path}'. Delete one!")
-						else: # The mod is not already in the cache
-							# Add it to the cache
-							most_updated_mods_cache[version][mod_ID] = {
-								"id": mod_ID,
-								"path": path
-							}
-
-			except Exception as e:
-				log(f"[{datetime.now().strftime('%m/%d/%Y %H:%M:%S:%f')}] - WARN: Error while caching most up to date mod '{mod_to_cache}'. {e}")
-
-	except Exception as e:
-		log(f"[{datetime.now().strftime('%m/%d/%Y %H:%M:%S:%f')}] - WARN: Unable to access most up to date mods directory '{config[version]['updated_mods_directory']} for'. {e}")
+			if mod_ID in most_updated_mods_cache[version]: # If the mod is already in the mod cache
+				# Remove the mod since there are two different versions
+				most_updated_mods_cache[version].pop(mod_ID)
+				log(f"[{datetime.now().strftime('%m/%d/%Y %H:%M:%S:%f')}] - WARN: Duplicate mod '{mod_ID}' found in most updated mods directory '{path}'. Delete one!")
+			else: # The mod is not already in the cache
+				# Add it to the cache
+				most_updated_mods_cache[version][mod_ID] = {
+					"id": mod_ID,
+					"path": str(path)
+				}
+except Exception as e:
+	log(f"[{datetime.now().strftime('%m/%d/%Y %H:%M:%S:%f')}] - WARN: {e}")
 #print(f"{most_updated_mods_cache = }")
 
 
 """UPDATING MODS"""
 log(f"[{datetime.now().strftime('%m/%d/%Y %H:%M:%S:%f')}] - INFO: UPDATING MODS...", print_to_console = False)
 count_mods_updated = 0
-for version in config.sections():
-	for directory in config[version]['mods_to_update_directories'].split(', '):
-		log(f"[{datetime.now().strftime('%m/%d/%Y %H:%M:%S:%f')}] - Updating mods in {directory}")
-		try:
+try:
+	for version in config.sections():
+		for directory in config[version]['mods_to_update_directories'].split(', '):
+			log(f"[{datetime.now().strftime('%m/%d/%Y %H:%M:%S:%f')}] - Updating mods in {directory}")
 			for mod in [mod for mod in os.listdir(directory) if mod.endswith(".jar")]:
-				try:
-					path = f"{directory}\\{mod}"
+				path = Path(f"{directory}\\{mod}")
+				modzip = ZipFile(path, "r")
+				modinfo = modzip.open("fabric.mod.json", "r")
+				info_json = json.load(modinfo, strict=False)
+				mod_ID = info_json["id"]
+				modinfo.close()
+				modzip.close()
 
-					with ZipFile(path, "r") as modzip:
-						with modzip.open("fabric.mod.json", "r") as modinfo:
-							info_json = json.load(modinfo, strict=False)
-							mod_ID = info_json["id"]
+				if mod_ID in most_updated_mods_cache[version]:
+					# If files are named the same, assume version is same already even though tmodified is different
+					if mod != os.path.basename(most_updated_mods_cache[version][mod_ID]['path']):
+						# Remove outdated mod
+						os.remove(str(path))
+						# Replace with more up to date version
+						shutil.copy2(most_updated_mods_cache[version][mod_ID]['path'], path.parent)
+						count_mods_updated += 1
+						log(f"INFO: Updated '{str(path)}'", print_to_console = False)
+				else:
+					log(f"[{datetime.now().strftime('%m/%d/%Y %H:%M:%S:%f')}] - WARN: Mod '{path}' does not have a copy within most up to date mods directory. It will be ignored.")
+except Exception as e:
+	log(f"[{datetime.now().strftime('%m/%d/%Y %H:%M:%S:%f')}] - WARN: {e}")
 
-						if mod_ID in most_updated_mods_cache[version]:
-							# If files are named the same, assume version is same already even though tmodified is different
-							if mod != os.path.basename(most_updated_mods_cache[version][mod_ID]['path']):
-								# Remove outdated mod
-								os.remove(path)
-								# Replace with more up to date version
-								shutil.copy2(most_updated_mods_cache[version][mod_ID]['path'], path)
-								count_mods_updated += 1
-								log(f"INFO: Updated '{path}'", print_to_console = False)
-						else:
-							log(f"WARN: Mod '{mod}' located at '{path}' does not have a copy within most up to date mods directory. It will be ignored.")
 
-				except Exception as e:
-					log(f"WARN: Error while processing '{mod}' for directory '{directory}'. {e}")
-
-		except Exception as e:
-			log(f"[{datetime.now().strftime('%m/%d/%Y %H:%M:%S:%f')}] - WARN: Unable to access mods to update directory '{directory}'")
 log(f"[{datetime.now().strftime('%m/%d/%Y %H:%M:%S:%f')}] - Updated {count_mods_updated} mod(s)")
-
 log(f"[{datetime.now().strftime('%m/%d/%Y %H:%M:%S:%f')}] - Done\n", print_to_console = False)
