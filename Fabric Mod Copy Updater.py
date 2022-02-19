947e49782b6fc9a225360faab27be8eacfacd7c0
Gianluca O <17207260+RandomGgames@users.noreply.github.com>
2022-02-19 04:28:57 -0500

    Update Fabric Mod Copy Updater.py
    Changed internal variable names and some logic

--- Fabric Mod Copy Updater.py
+++ Fabric Mod Copy Updater.py
@@ -18,11 +18,11 @@ def log(text, print_to_console: bool = True, print_to_log: bool = True):
 
 
 """LOG START TIME"""
-log(f"[{datetime.now().strftime('%m/%d/%Y %H:%M:%S:%f')}] - Running mods cleanup...")
+###log(f"[{datetime.now().strftime('%m/%d/%Y %H:%M:%S:%f')}] - Running mods cleanup...")
 
 
 """READ 'MOD UPDATER.INI' CONFIG"""
-log(f"[{datetime.now().strftime('%m/%d/%Y %H:%M:%S:%f')}] - INFO: READING CONFIG FILE...")
+###log(f"[{datetime.now().strftime('%m/%d/%Y %H:%M:%S:%f')}] - INFO: READING CONFIG FILE...")
 config = configparser.ConfigParser()
 # If the config file exists already, read it
 if os.path.isfile("mod updater.ini"):
@@ -31,67 +31,81 @@ if os.path.isfile("mod updater.ini"):
 else:
 	config['1.18 Mods'] = {
 		"updated_mods_directory": '',
-		"mods_to_update_directories": ''
+		"directories_to_keep_updated": ''
 	}
 	with open("mod updater.ini", "w") as configfile:
 		config.write(configfile)
 
 
 """BUILD MOST UP TO DATE MOD CACHE"""
-log(f"[{datetime.now().strftime('%m/%d/%Y %H:%M:%S:%f')}] - INFO: BUILDING MOST UP TO DATE MOD CACHE...")
-most_updated_mods_cache = {}
+log(f"[{datetime.now().strftime('%m/%d/%Y %H:%M:%S:%f')}] - INFO: BUILDING MOST UP TO DATE MOD CACHE...", print_to_log = False)
+most_updated_mods = {}
 try:
-	for version in config.sections():
-		most_updated_mods_cache[version] = {}
-		for mod_to_cache in [mod for mod in os.listdir(config[version]['updated_mods_directory']) if mod.endswith(".jar")]:
-			path = Path(f"{config[version]['updated_mods_directory']}\\{mod_to_cache}")
-			modzip = ZipFile(path, "r")
-			modinfo = modzip.open("fabric.mod.json", "r")
-			info_json = json.load(modinfo, strict=False)
-			mod_ID = info_json["id"]
-			modinfo.close()
+	for section in config.sections():
+		most_updated_mods[section] = {}
+		most_updated_mods_directory = Path(config.get(section, "updated_mods_directory"))
+		for mod in [file for file in os.listdir(str(most_updated_mods_directory)) if file.endswith(".jar")]:
+			path = Path(f"{str(most_updated_mods_directory)}\\{mod}")
+			tmodified = os.path.getmtime(str(path))
+			with ZipFile(path, "r") as modzip:
+				with modzip.open("fabric.mod.json", "r") as modinfo:
+					mod_id = json.load(modinfo, strict=False)["id"]
+				modinfo.close()
 			modzip.close()
-
-			if mod_ID in most_updated_mods_cache[version]: # If the mod is already in the mod cache
-				# Remove the mod since there are two different versions
-				most_updated_mods_cache[version].pop(mod_ID)
-				log(f"[{datetime.now().strftime('%m/%d/%Y %H:%M:%S:%f')}] - WARN: Duplicate mod '{mod_ID}' found in most updated mods directory '{path}'. Delete one!")
-			else: # The mod is not already in the cache
-				# Add it to the cache
-				most_updated_mods_cache[version][mod_ID] = {
-					"id": mod_ID,
-					"path": str(path)
+			
+			if mod_id not in most_updated_mods[section]:
+				most_updated_mods[section][mod_id] = {
+					"id": mod_id,
+					"path": str(path),
+					"tmodified": tmodified
 				}
+			else:
+				# If currently cached mod is older, replace with the new looped one.
+				current_tmodified = most_updated_mods[section][mod_id]['tmodified']
+				if current_tmodified < tmodified: #If the file cached is older
+					log(f"[{datetime.now().strftime('%m/%d/%Y %H:%M:%S:%f')}] - WARN: Outdated mod: {mod_id}, '{os.path.basename(most_updated_mods[section][mod_id]['path'])}'.")
+					most_updated_mods[section][mod_id] = {
+						"id": mod_id,
+						"path": str(path),
+						"tmodified": tmodified
+					}
+				else: #Current looped mod is outdated
+					log(f"[{datetime.now().strftime('%m/%d/%Y %H:%M:%S:%f')}] - WARN: Outdated mod: {mod_id}, '{os.path.basename(path)}'.")
+
+
 except Exception as e:
 	log(f"[{datetime.now().strftime('%m/%d/%Y %H:%M:%S:%f')}] - WARN: {e}")
-#print(f"{most_updated_mods_cache = }")
+#print(f"{most_updated_mods = }")
 
 
 """UPDATING MODS"""
-log(f"[{datetime.now().strftime('%m/%d/%Y %H:%M:%S:%f')}] - INFO: UPDATING MODS...")
+log(f"[{datetime.now().strftime('%m/%d/%Y %H:%M:%S:%f')}] - INFO: UPDATING MODS...", print_to_log = False)
 count_mods_updated = 0
 try:
-	for version in config.sections():
-		for directory in config[version]['mods_to_update_directories'].split(', '):
+	for section in config.sections():
+		directories_to_keep_updated = config[section]['directories_to_keep_updated'].split(', ')
+		for i, directory in enumerate(directories_to_keep_updated):
+			directories_to_keep_updated[i] = Path(directory)
+		for directory in directories_to_keep_updated:
 			log(f"[{datetime.now().strftime('%m/%d/%Y %H:%M:%S:%f')}] - Updating mods in {directory}")
 			for mod in [mod for mod in os.listdir(directory) if mod.endswith(".jar")]:
-				path = Path(f"{directory}\\{mod}")
-				modzip = ZipFile(path, "r")
-				modinfo = modzip.open("fabric.mod.json", "r")
-				info_json = json.load(modinfo, strict=False)
-				mod_ID = info_json["id"]
-				modinfo.close()
+				path = Path(f"{str(directory)}\\{mod}")
+				tmodified = os.path.getmtime(str(path))
+				with ZipFile(path, "r") as modzip:
+					with modzip.open("fabric.mod.json", "r") as modinfo:
+						mod_id = json.load(modinfo, strict=False)["id"]
+					modinfo.close()
 				modzip.close()
 
-				if mod_ID in most_updated_mods_cache[version]:
+				if mod_id in most_updated_mods[section]:
 					# If files are named the same, assume version is same already even though tmodified is different
-					if mod != os.path.basename(most_updated_mods_cache[version][mod_ID]['path']):
+					if mod != os.path.basename(most_updated_mods[section][mod_id]['path']):
 						# Remove outdated mod
 						os.remove(str(path))
 						# Replace with more up to date version
-						shutil.copy2(most_updated_mods_cache[version][mod_ID]['path'], path.parent)
+						shutil.copy2(most_updated_mods[section][mod_id]['path'], path.parent)
 						count_mods_updated += 1
-						log(f"INFO: Replaced '{str(path)}' with '{Path(most_updated_mods_cache[version][mod_ID]['path']).name}'")
+						log(f"INFO: Replaced '{str(path.name)}' with '{Path(most_updated_mods[section][mod_id]['path']).name}'")
 				else:
 					log(f"[{datetime.now().strftime('%m/%d/%Y %H:%M:%S:%f')}] - WARN: Mod '{path}' does not have a copy within most up to date mods directory. It will be ignored.")
 except Exception as e:
